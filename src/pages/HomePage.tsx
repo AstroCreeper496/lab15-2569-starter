7951c83d0d517f74c75ccf2ae9b459c511b2c6c0
AstroCreeper496_ <pluccoqlu.1927@gmail.com_>
2026-09-23 18:16:08 +0700

    adjust home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,6 +4,10 @@ import { Card } from "@/components/ui/card";
 
 export default function HomePage() {
   return( <div className="font-heading text-base leading-snug font-medium group-data-[size=sm]/card:text-sm">
-              <Card>ระบบลงทะเบียนเรียน CPE & ISNE</Card>
+              <Card >
+                <div className="mx-3">
+                  ระบบลงทะเบียนเรียน CPE & ISNE
+                </div>
+              </Card>
           </div>
 )}
